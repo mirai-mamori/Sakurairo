5a0311b4c3e0e0e1613e4b17596a54e6e1947557
染川 瞳 <61381142+mirai-mamori@users.noreply.github.com>
2020-03-31 22:59:44 +0800

    Add files via upload

--- js/sakura-app.js
+++ js/sakura-app.js
@@ -1917,3 +1917,12 @@ if ((isWebkit || isOpera || isIe) && document.getElementById && window.addEventL
         }
     }, false);
 }
+
+/* 首页下拉箭头 */
+function headertop_down() {
+    var coverOffset = $('#content').offset().top;
+    $('html,body').animate({
+        scrollTop: coverOffset
+    },
+    600);
+}
