1094a8805c6b1e5f17d814893bf84f22e6206f46
染川 瞳 <61381142+mirai-mamori@users.noreply.github.com>
2020-04-03 23:37:03 +0800

    Add files via upload

--- inc/decorate.php
+++ inc/decorate.php
@@ -92,6 +92,162 @@ $bg_style = akina_option('focus_height') ? 'background-position: center center;b
     -o-animation: rotating 6s linear infinite;
     animation: rotating 6s linear infinite;
 }
+#preload {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background: #ffffff;
+    z-index: 99999;
+}
+
+#preload li.active {
+    position: absolute;
+    top: 49%;
+    left: 49%;
+    list-style: none;
+}
+
+html {
+    overflow-y: hidden;
+}
+
+#preloader_3 {
+    position:relative;
+}
+#preloader_3:before {
+    width:20px;
+    height:20px;
+    border-radius:20px;
+    background:#EE9CA7;
+    content:'';
+    position:absolute;
+    background:#B5495B;
+    -webkit-animation: preloader_3_before 1.5s infinite ease-in-out;
+    -moz-animation: preloader_3_before 1.5s infinite ease-in-out;
+    -ms-animation: preloader_3_before 1.5s infinite ease-in-out;
+    animation: preloader_3_before 1.5s infinite ease-in-out;
+}
+#preloader_3:after {
+    width:20px;
+    height:20px;
+    border-radius:20px;
+    background:#EE9CA7;
+    content:'';
+    position:absolute;
+    background:#F4A7B9;
+    left:22px;
+    -webkit-animation: preloader_3_after 1.5s infinite ease-in-out;
+    -moz-animation: preloader_3_after 1.5s infinite ease-in-out;
+    -ms-animation: preloader_3_after 1.5s infinite ease-in-out;
+    animation: preloader_3_after 1.5s infinite ease-in-out;
+}
+@-webkit-keyframes preloader_3_before {
+    0% {
+        -webkit-transform: translateX(0px) rotate(0deg)
+    }
+    50% {
+        -webkit-transform: translateX(50px) scale(1.2) rotate(260deg);
+        background:#EE9CA7;
+        border-radius:0px;
+    }
+    100% {
+        -webkit-transform: translateX(0px) rotate(0deg)
+    }
+}
+@-webkit-keyframes preloader_3_after {
+    0% {
+        -webkit-transform: translateX(0px)
+    }
+    50% {
+        -webkit-transform: translateX(-50px) scale(1.2) rotate(-260deg);
+        background:#B5495B;
+        border-radius:0px;
+    }
+    100% {
+        -webkit-transform: translateX(0px)
+    }
+}
+@-moz-keyframes preloader_3_before {
+    0% {
+        -moz-transform: translateX(0px) rotate(0deg)
+    }
+    50% {
+        -moz-transform: translateX(50px) scale(1.2) rotate(260deg);
+        background:#EE9CA7;
+        border-radius:0px;
+    }
+    100% {
+        -moz-transform: translateX(0px) rotate(0deg)
+    }
+}
+@-moz-keyframes preloader_3_after {
+    0% {
+        -moz-transform: translateX(0px)
+    }
+    50% {
+        -moz-transform: translateX(-50px) scale(1.2) rotate(-260deg);
+        background:#B5495B;
+        border-radius:0px;
+    }
+    100% {
+        -moz-transform: translateX(0px)
+    }
+}
+@-ms-keyframes preloader_3_before {
+    0% {
+        -ms-transform: translateX(0px) rotate(0deg)
+    }
+    50% {
+        -ms-transform: translateX(50px) scale(1.2) rotate(260deg);
+        background:#EE9CA7;
+        border-radius:0px;
+    }
+    100% {
+        -ms-transform: translateX(0px) rotate(0deg)
+    }
+}
+@-ms-keyframes preloader_3_after {
+    0% {
+        -ms-transform: translateX(0px)
+    }
+    50% {
+        -ms-transform: translateX(-50px) scale(1.2) rotate(-260deg);
+        background:#B5495B;
+        border-radius:0px;
+    }
+    100% {
+        -ms-transform: translateX(0px)
+    }
+}
+@keyframes preloader_3_before {
+    0% {
+        transform: translateX(0px) rotate(0deg)
+    }
+    50% {
+        transform: translateX(50px) scale(1.2) rotate(260deg);
+        background:#EE9CA7;
+        border-radius:0px;
+    }
+    100% {
+        transform: translateX(0px) rotate(0deg)
+    }
+}
+@keyframes preloader_3_after {
+    0% {
+        transform: translateX(0px)
+    }
+    50% {
+        transform: translateX(-50px) scale(1.2) rotate(-260deg);
+        background:#B5495B;
+        border-radius:0px;
+    }
+    100% {
+        transform: translateX(0px)
+    }
+}
+
 <?php if(akina_option('comment_info_box_width', '')): ?>
 .cmt-popup {
     --widthA: <?php echo akina_option('comment_info_box_width', ''); ?>%;
