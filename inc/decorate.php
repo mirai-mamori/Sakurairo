f8850e8a6a74f0b99936f610b8af97378324badf
染川 瞳 <61381142+mirai-mamori@users.noreply.github.com>
2020-04-12 21:09:22 +0800

    Add files via upload

--- inc/decorate.php
+++ inc/decorate.php
@@ -647,12 +647,12 @@ body.dark .entry-content p,body.dark .entry-content ul,body.dark .entry-content 
 body.dark .entry-title a,body.dark .post-list-thumb .post-title,body.dark .post-list-thumb,body.dark .art-content #archives .al_mon_list .al_mon,body.dark .art-content #archives .al_mon_list span,body.dark .art .art-content #archives a,body.dark .menhera-container .emoji-item{color:#bebebe;}
 body.dark .site-top ul li a,body.dark .header-user-menu a,body.dark #mo-nav ul li a,body.dark .site-title a,body.dark header.page-header,body.dark h1.cat-title{color:#eee;}
 body.dark .art .art-content #archives .al_year,body.dark .comment-respond input,body.dark .comment-respond textarea,body.dark .siren-checkbox-label{color:#eee;}
-body.dark .post-date,body.dark .post-list-thumb a,body.dark .post-meta{color:#888;}
+body.dark .post-date,body.dark .post-list-thumb a,body.dark .post-meta,body.dark .info-meta a,body.dark .info-meta span{color:#888;}
 body.dark img,body.dark .highlight-wrap,body.dark iframe,body.dark .entry-content .aplayer{filter:brightness(0.6);}
 body.dark .post-list-thumb{box-shadow:0 1px 35px -8px rgba(0,0,0,0.8);}
 body.dark .notice{color:#EFF0F1;background:#232629;border:none;}
 body.dark h1.fes-title,body.dark h1.main-title{border-bottom:6px dotted #ababab;}
-body.dark #moblieGoTop,body.dark .notification,body.dark #changskin{color:#eee;background-color:#232629;}
+body.dark #moblieGoTop,body.dark .notification,body.dark .the-feature.from_left_and_right .info,body.dark #changskin{color:#eee;background-color:#232629;}
 body.dark #moblieGoTop:hover,body.dark #changskin:hover{background-color:#232629;opacity:.8;}
 body.dark .widget-area{background-color:rgba(35,38,41,0.8);}
 body.dark .skin-menu,body.dark .menu-list li,body.dark .widget-area .heading,body.dark .widget-area .show-hide svg,body.dark #aplayer-float,body.dark .aplayer.aplayer-fixed .aplayer-body,body.dark .aplayer .aplayer-miniswitcher,body.dark .aplayer .aplayer-pic{color:#eee;background-color:#232629 !important;}
@@ -666,6 +666,7 @@ body.dark .yya{box-shadow: 0 1px 40px -8px #21252b;}
 body.dark .font-family-controls button.selected{background-color:#31363b;}
 body.dark .user-menu-option a:hover{background-color:#535a63;}
 body.dark .font-family-controls button{background-color:#535a63;}
+body.dark #banner_wave_1,body.dark #banner_wave_2{display:none;}
 body.dark .ex-login-username,body.dark .admin-login-check p,body.dark .user-login-check p,body.dark .ex-logout a, .ex-new-account a{color:#bebebe;}
 body.dark .aplayer .aplayer-info .aplayer-controller .aplayer-time .aplayer-icon:hover path,body.dark .widget-area .show-hide svg path{fill:#fafafa;}
 body.dark,body.dark button,body.dark input,body.dark select,body.dark textarea{color:#eee;}
