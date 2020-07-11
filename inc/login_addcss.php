eab610a2747f60c64b5f396c8aee5790d169c974
Ukenn <duola2002@gmail.com>
2020-07-11 09:47:20 +0800

    登录界面按钮主题色跟随

---
+++ inc/login_addcss.php
@@ -0,0 +1,14 @@
+<style type="text/css">
+.forgetmenot input:checked + label {
+    background: <?php echo akina_option('theme_skin'); ?>;
+}
+#labelTip {
+    background-color: <?php echo akina_option('theme_skin'); ?>;
+}
+#label {
+    color: <?php echo akina_option('theme_skin'); ?>;
+}
+#login .submit .button {
+    background: <?php echo akina_option('theme_skin'); ?>;
+}
+</style>
