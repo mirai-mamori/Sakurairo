e5d917284983d4aca811bb4a4255ae4bda64e2a3
染川 瞳 <61381142+mirai-mamori@users.noreply.github.com>
2020-08-06 16:01:36 +0800

    Delete Useless Function

--- functions.php
+++ functions.php
@@ -937,16 +937,6 @@ function comment_mail_notify($comment_id)
 }
 add_action('comment_post', 'comment_mail_notify');
 
-//字数统计
-function count_words ($text) {
-global $post;
-if ( '' == $text ) {
-$text = $post->post_content;
-if (mb_strlen($output, 'UTF-8') < mb_strlen($text, 'UTF-8')) $output .= '本文共' . mb_strlen(preg_replace('/\s/','',html_entity_decode(strip_tags($post->post_content))),'UTF-8') . '个字';
-return $output;
-}
-}
-
 /*
  * 链接新窗口打开
  */
