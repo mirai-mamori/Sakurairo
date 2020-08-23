a2935b4c3d25e81db76025a6b9b46bfa411a3b2d
染川 瞳 <61381142+mirai-mamori@users.noreply.github.com>
2020-08-23 16:12:02 +0800

    Seventh Moon Update Step 2

--- update-checker/update-checker.php
+++ update-checker/update-checker.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Plugin Update Checker Library 4.9
+ * Plugin Update Checker Library 4.10
  * http://w-shadow.com/
  *
  * Copyright 2020 Janis Elsts
  * Released under the MIT license. See license.txt for details.
  */
 
-require dirname(__FILE__) . '/load-v4p9.php';
+require dirname(__FILE__) . '/load-v4p10.php';
