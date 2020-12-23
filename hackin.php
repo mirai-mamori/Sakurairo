f6e777ef8d454dff09bba0b0cee1a3f989d55001
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-12-23 21:16:48 +0800

    update pv

--- hackin.php
+++ hackin.php
@@ -1,6 +1,6 @@
 <?php
 function hack_pv($word="View"){
-    return '<span class="meta-page-view" data-path="'.get_permalink(get_the_ID()).'">'
+    return '<span class="meta-page-view" data-path="'.parse_url(get_permalink(get_the_ID()))['path'].'">'
     .get_post_views(get_the_ID()) . ' ' 
     . _n($word, $word.'s', get_post_views(get_the_ID()), 'sakurairo')/*次阅读*/ .'</span>';
 }
--- tpl/content-thumb.php
+++ tpl/content-thumb.php
@@ -12,6 +12,7 @@
 //}
 //add_filter('the_excerpt', 'custom_short_excerpt');
 $i = 0;
+require_once(get_template_directory() .'/hackin.php');
 while (have_posts()) : the_post();
 	$i++;
 	if ($i == 1) {
@@ -52,7 +53,7 @@ while (have_posts()) : the_post();
 					wp_enqueue_script('r17-dom', "https://cdn.jsdelivr.net/npm/react-dom@17.0.1/umd/react-dom.production.min.js", array(), null, true);
 					wp_enqueue_script('pv',  "https://cdn.jsdelivr.net/gh/kotorik/yukicat-attach@s1.1/dist/pv.js", array('r17', 'r17-dom'), null, true);
 					wp_enqueue_style("pv_style", "https://cdn.jsdelivr.net/gh/kotorik/yukicat-attach@latest/dist/pv.css") ?>
-					<span><i class="iconfont icon-attention"></i><?php require_once(get_template_directory() .'/hackin.php');echo hack_pv(); ?></span></span>
+					<span><i class="iconfont icon-attention"></i><?php echo hack_pv('Hit'); ?></span></span>
 					<span class="comments-number"><i class="iconfont icon-mark"></i><?php comments_popup_link('NOTHING', '1 ' . __("Comment", "sakurairo")/*条评论*/, '% ' . __("Comments", "sakurairo")/*条评论*/); ?></span>
 					<span><i class="iconfont icon-file"></i><a href="<?php echo esc_url(get_category_link($the_cat[0]->cat_ID)); ?>"><?php echo $the_cat[0]->cat_name; ?></a>
 					</span>
