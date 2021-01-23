37b37c412be3fa7f436b30dd13372f71a5d6f843
染川 瞳 <61381142+mirai-mamori@users.noreply.github.com>
2021-01-23 11:24:13 +0800

    Daily Update

--- opt/options/theme-options.php
+++ opt/options/theme-options.php
@@ -1596,7 +1596,7 @@ if( class_exists( 'CSF' ) ) {
         'type'   => 'text',
         'title'  => '公告文本',
         'dependency' => array( 'announce_bar', '==', 'true' ),
-        'desc'   => '填写公告文本，文本超出142个字节将会被滚动显示',
+        'desc'   => '填写公告文本，文本超出142个字节将会被隐藏',
       ),
 
       array(
