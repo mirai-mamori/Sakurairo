19d9c39fd19f616046ee4d58e1f324d8c91d1735
bymoye <964907582@qq.com>
2021-03-17 20:56:34 +0800

    评论图片上传区

--- js/sakura-app.js
+++ js/sakura-app.js
@@ -41,8 +41,8 @@ function setCookie(name, value, days) {
 }
 
 function getCookie(name) {
-    let nameEQ = name + mashiro_option.cookie_version_control + "=";
-    let ca = document.cookie.split(';');
+    let nameEQ = name + mashiro_option.cookie_version_control + "=",
+        ca = document.cookie.split(';');
     for (let i = 0; i < ca.length; i++) {
         let c = ca[i];
         while (c.charAt(0) == ' ') c = c.substring(1, c.length);
@@ -157,24 +157,27 @@ function code_highlight_style() {
             'contenteditable': 'false',
             'design': 'by Mashiro'
         },
-        ele_name = pre[i].children[0].className,
+        ele_name = pre[i]?.children[0]?.className,
              lang = ele_name.substr(0, ele_name.indexOf(" ")).replace('language-', ''),
              code_a = code[i];
              if (lang.toLowerCase() == "hljs") lang = code_a.className.replace('hljs', '') ? code_a.className.replace('hljs', '') : "text";
-        $('pre:eq(' + i + ')').addClass('highlight-wrap');
+        pre[i].classList.add("highlight-wrap");
         for (let t in attributes) {
             pre[i].setAttribute(t,attributes[t]);
         }
         code_a.setAttribute('data-rel', lang.toUpperCase());
     }
-    code.forEach((block) => {
-        hljs.highlightBlock(block);
-    })
+    for (let i = 0; i < code.length; i++) {
+        hljs.highlightBlock(code[i]);
+    }
+    // code.forEach((block) => {
+    //     hljs.highlightBlock(block);
+    // })
     for (let i = 0; i < pre.length; i++) {
         gen_top_bar(i);
     }
     hljs.initLineNumbersOnLoad();
-    document.body.addEventListener("click",function(e){
+    document.getElementsByClassName("entry-content")[0]?.addEventListener("click",function(e){
         if (!e.target.classList.contains("highlight-wrap"))return;
         e.target.classList.toggle("code-block-fullscreen");
         document.getElementsByTagName("html")[0].classList.toggle('code-block-fullscreen-html-scroll');
@@ -185,94 +188,188 @@ try {
 } catch (e) {}
 
 if (Poi.reply_link_version == 'new') {
-    $('body').on('click', '.comment-reply-link', function () {
-        addComment.moveForm("comment-" + $(this).attr('data-commentid'), $(this).attr('data-commentid'), "respond", $(this).attr('data-postid'));
-        return false;
-    });
+    document.getElementsByClassName("comments-main")[0]?.addEventListener("click", function (e) {
+        if (e.target.classList.contains("comment-reply-link")) {
+            e.preventDefault();
+            e.stopPropagation();
+            let data_commentid = e.target.getAttribute("data-commentid");
+            addComment.moveForm("comment-" + data_commentid, data_commentid, "respond", this.getAttribute("data-postid"));
+        }
+    })
+    // $('body').on('click', '.comment-reply-link', function () {
+    //     addComment.moveForm("comment-" + $(this).attr('data-commentid'), $(this).attr('data-commentid'), "respond", $(this).attr('data-postid'));
+    //     return false;
+    // });
 }
 
 function attach_image() {
-    var cached = $('.insert-image-tips');
-    $('#upload-img-file').change(function () {
+    let cached = document.getElementsByClassName("insert-image-tips")[0],
+        upload_img = document.getElementById('upload-img-file');
+    upload_img?.addEventListener("change", (function () {
         if (this.files.length > 10) {
             addComment.createButterbar("每次上传上限为10张.<br>10 files max per request.");
             return 0;
         }
-        for (i = 0; i < this.files.length; i++) {
+        for (let i = 0; i < this.files.length; i++) {
             if (this.files[i].size >= 5242880) {
-                alert('图片上传大小限制为5 MB.\n5 MB max per file.\n\n「' + this.files[i].name + '」\n\n这张图太大啦~\nThis image is too large~');
+                alert('图片上传大小限制为5 MB.\n5 MB max per file.\n\n「' + this.files[i].name + '」\n\n这张图太大啦~请重新上传噢！\nThis image is too large~Please reupload!');
+                return;
             }
         }
-        for (var i = 0; i < this.files.length; i++) {
-            var f = this.files[i];
-            var formData = new FormData();
+        for (let i = 0; i < this.files.length; i++) {
+            let f = this.files[i],
+                formData = new FormData(),
+                xhr = new XMLHttpRequest();
             formData.append('cmt_img_file', f);
-            $.ajax({
-                url: Poi.api + 'sakura/v1/image/upload?_wpnonce=' + Poi.nonce,
-                type: 'POST',
-                processData: false,
-                contentType: false,
-                data: formData,
-                beforeSend: function (xhr) {
-                    cached.html('<i class="fa fa-spinner rotating" aria-hidden="true"></i>');
-                    addComment.createButterbar("上传中...<br>Uploading...");
-                },
-                success: function (res) {
-                    cached.html('<i class="fa fa-check" aria-hidden="true"></i>');
+            xhr.addEventListener('loadstart', function () {
+                cached.innerHTML = '<i class="fa fa-spinner rotating" aria-hidden="true"></i>';
+                addComment.createButterbar("上传中...<br>Uploading...");
+            });
+            xhr.open("POST", Poi.api + 'sakura/v1/image/upload?_wpnonce=' + Poi.nonce, true);
+            xhr.send(formData);
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 304)) {
+                    cached.innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>';
                     setTimeout(function () {
-                        cached.html('<i class="fa fa-picture-o" aria-hidden="true"></i>');
+                        cached.innerHTML = '<i class="fa fa-picture-o" aria-hidden="true"></i>';
                     }, 1000);
+                    let res = JSON.parse(xhr.responseText);
                     if (res.status == 200) {
-                        var get_the_url = res.proxy;
-                        $('#upload-img-show').append('<img class="lazyload upload-image-preview" src="https://cdn.jsdelivr.net/gh/Fuukei/Public_Repository@latest/vision/theme/colorful/load/inload.svg" data-src="' + get_the_url + '" onclick="window.open(\'' + get_the_url + '\')" onerror="imgError(this)" />');
+                        let get_the_url = res.proxy;
+                        document.getElementById("upload-img-show").insertAdjacentHTML('afterend', '<img class="lazyload upload-image-preview" src="https://cdn.jsdelivr.net/gh/Fuukei/Public_Repository@latest/vision/theme/colorful/load/inload.svg" data-src="' + get_the_url + '" onclick="window.open(\'' + get_the_url + '\')" onerror="imgError(this)" />');
                         lazyload();
                         addComment.createButterbar("图片上传成功~<br>Uploaded successfully~");
                         grin(get_the_url, type = 'Img');
                     } else {
-                        addComment.createButterbar("上传失败！<br>Uploaded failed!<br> 文件名/Filename: "+f.name+"<br>code: "+res.status+"<br>"+res.message, 3000);
+                        addComment.createButterbar("上传失败！<br>Uploaded failed!<br> 文件名/Filename: " + f.name + "<br>code: " + res.status + "<br>" + res.message, 3000);
                     }
-                },
-                error: function (jqXHR, textStatus, errorThrown) {
-                    cached.html('<i class="fa fa-times" aria-hidden="true" style="color:red"></i>');
+                } else if (xhr.readyState == 4) {
+                    cached.innerHTML = '<i class="fa fa-times" aria-hidden="true" style="color:red"></i>';
                     alert("上传失败，请重试.\nUpload failed, please try again.");
                     setTimeout(function () {
-                        cached.html('<i class="fa fa-picture-o" aria-hidden="true"></i>');
+                        cached.innerHTML = '<i class="fa fa-picture-o" aria-hidden="true"></i>';
                     }, 1000);
-                    // console.info(jqXHR.responseText);
-                    // console.info(jqXHR.status);
-                    // console.info(jqXHR.readyState);
-                    // console.info(jqXHR.statusText);
-                    // console.info(textStatus);
-                    // console.info(errorThrown);
                 }
-            })
-        }
-    });
+            }
+        };
+    }));
 }
+
+// function attach_image() {
+//     var cached = $('.insert-image-tips');
+//     $('#upload-img-file').change(function () {
+//         if (this.files.length > 10) {
+//             addComment.createButterbar("每次上传上限为10张.<br>10 files max per request.");
+//             return 0;
+//         }
+//         for (i = 0; i < this.files.length; i++) {
+//             if (this.files[i].size >= 5242880) {
+//                 alert('图片上传大小限制为5 MB.\n5 MB max per file.\n\n「' + this.files[i].name + '」\n\n这张图太大啦~\nThis image is too large~');
+//             }
+//         }
+//         for (var i = 0; i < this.files.length; i++) {
+//             var f = this.files[i];
+//             var formData = new FormData();
+//             formData.append('cmt_img_file', f);
+//             $.ajax({
+//                 url: Poi.api + 'sakura/v1/image/upload?_wpnonce=' + Poi.nonce,
+//                 type: 'POST',
+//                 processData: false,
+//                 contentType: false,
+//                 data: formData,
+//                 beforeSend: function (xhr) {
+//                     cached.html('<i class="fa fa-spinner rotating" aria-hidden="true"></i>');
+//                     addComment.createButterbar("上传中...<br>Uploading...");
+//                 },
+//                 success: function (res) {
+//                     cached.html('<i class="fa fa-check" aria-hidden="true"></i>');
+//                     setTimeout(function () {
+//                         cached.html('<i class="fa fa-picture-o" aria-hidden="true"></i>');
+//                     }, 1000);
+//                     if (res.status == 200) {
+//                         var get_the_url = res.proxy;
+//                         $('#upload-img-show').append('<img class="lazyload upload-image-preview" src="https://cdn.jsdelivr.net/gh/Fuukei/Public_Repository@latest/vision/theme/colorful/load/inload.svg" data-src="' + get_the_url + '" onclick="window.open(\'' + get_the_url + '\')" onerror="imgError(this)" />');
+//                         lazyload();
+//                         addComment.createButterbar("图片上传成功~<br>Uploaded successfully~");
+//                         grin(get_the_url, type = 'Img');
+//                     } else {
+//                         addComment.createButterbar("上传失败！<br>Uploaded failed!<br> 文件名/Filename: "+f.name+"<br>code: "+res.status+"<br>"+res.message, 3000);
+//                     }
+//                 },
+//                 error: function (jqXHR, textStatus, errorThrown) {
+//                     cached.html('<i class="fa fa-times" aria-hidden="true" style="color:red"></i>');
+//                     alert("上传失败，请重试.\nUpload failed, please try again.");
+//                     setTimeout(function () {
+//                         cached.html('<i class="fa fa-picture-o" aria-hidden="true"></i>');
+//                     }, 1000);
+//                     // console.info(jqXHR.responseText);
+//                     // console.info(jqXHR.status);
+//                     // console.info(jqXHR.readyState);
+//                     // console.info(jqXHR.statusText);
+//                     // console.info(textStatus);
+//                     // console.info(errorThrown);
+//                 }
+//             })
+//         }
+//     });
+// }
 
 function clean_upload_images() {
-    $('#upload-img-show').html('');
+    document.getElementById("upload-img-show").innerHTML = '';
 }
 
+// function clean_upload_images() {
+//     $('#upload-img-show').html('');
+// }
 function add_upload_tips() {
-    $('<div class="insert-image-tips popup"><i class="fa fa-picture-o" aria-hidden="true"></i><span class="insert-img-popuptext" id="uploadTipPopup">上传图片</span></div><input id="upload-img-file" type="file" accept="image/*" multiple="multiple" class="insert-image-button">').insertAfter($(".form-submit #submit"));
+    let form_subit = document.querySelector('.form-submit #submit');
+    if (form_subit == null) return;
+    form_subit.insertAdjacentHTML('afterend', '<div class="insert-image-tips popup"><i class="fa fa-picture-o" aria-hidden="true"></i><span class="insert-img-popuptext" id="uploadTipPopup">上传图片</span></div><input id="upload-img-file" type="file" accept="image/*" multiple="multiple" class="insert-image-button">');
     attach_image();
-    $("#upload-img-file").hover(function () {
-        $(".insert-image-tips").addClass("insert-image-tips-hover");
-        $("#uploadTipPopup").addClass("show");
-    }, function () {
-        $(".insert-image-tips").removeClass("insert-image-tips-hover");
-        $("#uploadTipPopup").removeClass("show");
+    let file_subit = document.getElementById('upload-img-file'),
+        hover = document.getElementsByClassName('insert-image-tips')[0],
+        Tip = document.getElementById('uploadTipPopup');
+    file_subit?.addEventListener("mouseenter", function () {
+        hover.classList.toggle('insert-image-tips-hover');
+        Tip.classList.toggle('show');
+    });
+    file_subit?.addEventListener("mouseleave", function () {
+        hover.classList.toggle('insert-image-tips-hover');
+        Tip.classList.toggle('show');
     });
 }
+
+// function add_upload_tips() {
+//     $('<div class="insert-image-tips popup"><i class="fa fa-picture-o" aria-hidden="true"></i><span class="insert-img-popuptext" id="uploadTipPopup">上传图片</span></div><input id="upload-img-file" type="file" accept="image/*" multiple="multiple" class="insert-image-button">').insertAfter($(".form-submit #submit"));
+//     attach_image();
+//     $("#upload-img-file").hover(function () {
+//         $(".insert-image-tips").addClass("insert-image-tips-hover");
+//         $("#uploadTipPopup").addClass("show");
+//     }, function () {
+//         $(".insert-image-tips").removeClass("insert-image-tips-hover");
+//         $("#uploadTipPopup").removeClass("show");
+//     });
+// }
 
 function click_to_view_image() {
-    $(".comment_inline_img").click(function () {
-        var temp_url = this.src;
-        window.open(temp_url);
-    });
+    let comment_inline = document.getElementsByClassName('comment_inline_img');
+    if (comment_inline.length == 0) return;
+    document.getElementsByClassName("comments-main")[0]?.addEventListener("click", function (e) {
+        if (e.target.classList.contains("comment_inline_img")) {
+            let temp_url = e.target.src;
+            window.open(temp_url);
+        }
+    })
 }
 click_to_view_image();
+
+// function click_to_view_image() {
+//     $(".comment_inline_img").click(function () {
+//         var temp_url = this.src;
+//         window.open(temp_url);
+//     });
+// }
+// click_to_view_image();
 
 function original_emoji_click() {
     $(".emoji-item").click(function () {
