23ec8d5553d93f9fbe3fdf466f60e5506eb38c46
bymoye <964907582@qq.com>
2021-03-27 15:02:25 +0800

    移除部分注释

--- js/sakura-app.js
+++ js/sakura-app.js
@@ -143,9 +143,6 @@ mashiro_global.font_control = new function () {
             document.body.classList.add("serif");
             document.getElementsByClassName("control-btn-serif")[0]?.classList.add("selected");
             document.getElementsByClassName("control-btn-sans-serif")[0]?.classList.remove("selected");
-            // $("body").addClass("serif");
-            // $(".control-btn-serif").addClass("selected");
-            // $(".control-btn-sans-serif").removeClass("selected");
             setCookie("font_family", "serif", 30);
             if (document.body.clientWidth <= 860) {
                 addComment.createButterbar("将从网络加载字体，流量请注意");
@@ -162,9 +159,6 @@ mashiro_global.font_control = new function () {
             document.body.classList.remove("sans-serif");
             document.getElementsByClassName("control-btn-serif")[0]?.classList.remove("selected");
             document.getElementsByClassName("control-btn-sans-serif")[0]?.classList.add("selected");
-            // $("body").removeClass("sans-serif");
-            // $(".control-btn-serif").removeClass("selected");
-            // $(".control-btn-sans-serif").addClass("selected");
         }
     }
 }
@@ -195,9 +189,6 @@ function code_highlight_style() {
     for (let i = 0; i < code.length; i++) {
         hljs.highlightBlock(code[i]);
     }
-    // code.forEach((block) => {
-    //     hljs.highlightBlock(block);
-    // })
     for (let i = 0; i < pre.length; i++) {
         gen_top_bar(i);
     }
@@ -221,10 +212,6 @@ if (Poi.reply_link_version == 'new') {
             addComment.moveForm("comment-" + data_commentid, data_commentid, "respond", this.getAttribute("data-postid"));
         }
     })
-    // $('body').on('click', '.comment-reply-link', function () {
-    //     addComment.moveForm("comment-" + $(this).attr('data-commentid'), $(this).attr('data-commentid'), "respond", $(this).attr('data-postid'));
-    //     return false;
-    // });
 }
 let ready = function(fn){
     if (typeof fn !== 'function') return;
@@ -287,72 +274,11 @@ function attach_image() {
     }));
 }
 
-// function attach_image() {
-//     var cached = $('.insert-image-tips');
-//     $('#upload-img-file').change(function () {
-//         if (this.files.length > 10) {
-//             addComment.createButterbar("每次上传上限为10张.<br>10 files max per request.");
-//             return 0;
-//         }
-//         for (i = 0; i < this.files.length; i++) {
-//             if (this.files[i].size >= 5242880) {
-//                 alert('图片上传大小限制为5 MB.\n5 MB max per file.\n\n「' + this.files[i].name + '」\n\n这张图太大啦~\nThis image is too large~');
-//             }
-//         }
-//         for (var i = 0; i < this.files.length; i++) {
-//             var f = this.files[i];
-//             var formData = new FormData();
-//             formData.append('cmt_img_file', f);
-//             $.ajax({
-//                 url: Poi.api + 'sakura/v1/image/upload?_wpnonce=' + Poi.nonce,
-//                 type: 'POST',
-//                 processData: false,
-//                 contentType: false,
-//                 data: formData,
-//                 beforeSend: function (xhr) {
-//                     cached.html('<i class="fa fa-spinner rotating" aria-hidden="true"></i>');
-//                     addComment.createButterbar("上传中...<br>Uploading...");
-//                 },
-//                 success: function (res) {
-//                     cached.html('<i class="fa fa-check" aria-hidden="true"></i>');
-//                     setTimeout(function () {
-//                         cached.html('<i class="fa fa-picture-o" aria-hidden="true"></i>');
-//                     }, 1000);
-//                     if (res.status == 200) {
-//                         var get_the_url = res.proxy;
-//                         $('#upload-img-show').append('<img class="lazyload upload-image-preview" src="https://cdn.jsdelivr.net/gh/Fuukei/Public_Repository@latest/vision/theme/colorful/load/inload.svg" data-src="' + get_the_url + '" onclick="window.open(\'' + get_the_url + '\')" onerror="imgError(this)" />');
-//                         lazyload();
-//                         addComment.createButterbar("图片上传成功~<br>Uploaded successfully~");
-//                         grin(get_the_url, type = 'Img');
-//                     } else {
-//                         addComment.createButterbar("上传失败！<br>Uploaded failed!<br> 文件名/Filename: "+f.name+"<br>code: "+res.status+"<br>"+res.message, 3000);
-//                     }
-//                 },
-//                 error: function (jqXHR, textStatus, errorThrown) {
-//                     cached.html('<i class="fa fa-times" aria-hidden="true" style="color:red"></i>');
-//                     alert("上传失败，请重试.\nUpload failed, please try again.");
-//                     setTimeout(function () {
-//                         cached.html('<i class="fa fa-picture-o" aria-hidden="true"></i>');
-//                     }, 1000);
-//                     // console.info(jqXHR.responseText);
-//                     // console.info(jqXHR.status);
-//                     // console.info(jqXHR.readyState);
-//                     // console.info(jqXHR.statusText);
-//                     // console.info(textStatus);
-//                     // console.info(errorThrown);
-//                 }
-//             })
-//         }
-//     });
-// }
 
 function clean_upload_images() {
     document.getElementById("upload-img-show").innerHTML = '';
 }
 
-// function clean_upload_images() {
-//     $('#upload-img-show').html('');
-// }
 function add_upload_tips() {
     let form_subit = document.querySelector('.form-submit #submit');
     if (form_subit == null) return;
@@ -371,18 +297,6 @@ function add_upload_tips() {
     });
 }
 
-// function add_upload_tips() {
-//     $('<div class="insert-image-tips popup"><i class="fa fa-picture-o" aria-hidden="true"></i><span class="insert-img-popuptext" id="uploadTipPopup">上传图片</span></div><input id="upload-img-file" type="file" accept="image/*" multiple="multiple" class="insert-image-button">').insertAfter($(".form-submit #submit"));
-//     attach_image();
-//     $("#upload-img-file").hover(function () {
-//         $(".insert-image-tips").addClass("insert-image-tips-hover");
-//         $("#uploadTipPopup").addClass("show");
-//     }, function () {
-//         $(".insert-image-tips").removeClass("insert-image-tips-hover");
-//         $("#uploadTipPopup").removeClass("show");
-//     });
-// }
-
 function click_to_view_image() {
     let comment_inline = document.getElementsByClassName('comment_inline_img');
     if (comment_inline.length == 0) return;
@@ -395,13 +309,6 @@ function click_to_view_image() {
 }
 click_to_view_image();
 
-// function click_to_view_image() {
-//     $(".comment_inline_img").click(function () {
-//         var temp_url = this.src;
-//         window.open(temp_url);
-//     });
-// }
-// click_to_view_image();
 
 function original_emoji_click() {
     document.querySelector(".menhera-container")?.addEventListener("click", function (e) {
@@ -409,16 +316,9 @@ function original_emoji_click() {
             grin(e.target.innerText, "custom", "`", "` ");
         }
     })
-    // $(".emoji-item").click(function () {
-    //     grin($(this).text(), type = "custom", before = "`", after = "` ");
-    // });
 }
 original_emoji_click();
 
-// function showPopup(ele) {
-//     var popup = ele.querySelector("#thePopup");
-//     popup.classList.toggle("show");
-// }
 
 function cmt_showPopup(ele) {
     let popup = ele.querySelector("#thePopup");
@@ -426,11 +326,6 @@ function cmt_showPopup(ele) {
     ele.querySelector("input").onblur = ()=>{
         popup.classList.remove("show");
     }
-    // var popup = $(ele).find("#thePopup");
-    // popup.addClass("show");
-    // $(ele).find("input").blur(function () {
-    //     popup.removeClass("show");
-    // });
 }
 
 function scrollBar() {
@@ -458,32 +353,6 @@ function scrollBar() {
         }
         document.querySelector(".skin-menu")?.classList.remove("show");
         })
-        // $(window).scroll(function () {
-        //     var s = $(window).scrollTop(),
-        //         a = $(document).height(),
-        //         b = $(window).height(),
-        //         result = parseInt(s / (a - b) * 100),
-        //         cached = $("#bar");
-        //     cached.css("width", result + "%");
-        //     if (false) {
-        //         if (result >= 0 && result <= 19)
-        //             cached.css("background", "#cccccc");
-        //         if (result >= 20 && result <= 39)
-        //             cached.css("background", "#50bcb6");
-        //         if (result >= 40 && result <= 59)
-        //             cached.css("background", "#85c440");
-        //         if (result >= 60 && result <= 79)
-        //             cached.css("background", "#f2b63c");
-        //         if (result >= 80 && result <= 99)
-        //             cached.css("background", "#FF0000");
-        //         if (result == 100)
-        //             cached.css("background", "#5aaadb");
-        //     } else {
-        //         cached.css("background", "orange");
-        //     }
-        //     $(".toc-container").css("height", $(".site-content").outerHeight());
-        //     $(".skin-menu").removeClass('show');
-        // });
     }
 }
 
