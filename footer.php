ddbfb396e9a148536f87e2e5757abfaf520b2848
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-05-26 18:58:27 +0800

    Revert "fix: openNav中site-branding未渲染"
    This reverts commit d71ff338ffd8d32c24c0cf51efef8fe5bf97fbe9.

--- footer.php
+++ footer.php
@@ -45,7 +45,13 @@ $reception_background = iro_opt('reception_background');
 		<div class="iconflat no-select">	 
 			<div class="icon"></div>
 		</div>
-		<?php require_once get_stylesheet_directory().'/inc/site-branding.php'; print_site_branding();?>
+		<div class="site-branding">
+			<?php if (iro_opt('iro_logo')){ ?>
+			<div class="site-title">
+			<?php }else{ ?>
+			<h1 class="site-title"></h1>
+			<?php } ?>
+		</div>
 	</div><!-- m-nav-bar -->
 	</section><!-- #section -->
 	<!-- m-nav-center -->
