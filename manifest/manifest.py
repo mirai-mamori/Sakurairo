b904f0bda24c16f5791b421294ce60b29b2203c1
bymoye <964907582@qq.com>
2021-06-09 00:28:39 +0800

    refactor: manifest程序重构(多线程支持,合并程序,自动分离两端图片[图片宽高判断])

--- manifest/manifest.py
+++ manifest/manifest.py
@@ -1,84 +1,90 @@
 # coding=utf-8
 '''
-Created on Apr 23, 2018
-Desc: Webp convertor
-@author: Mashiro https://2heng.xin
+Created on Jun 6, 2021
+Desc: image process
+@author: Bymoye
 '''
 import os
-import sys
-import json
-import hashlib
 from PIL import Image
+from concurrent import futures
+import hashlib
+import json
+from time import *
 
-class Single(object):
-  def __init__(self, file, manifest):
-    self.file = file
-    self.mani = manifest
-
-
-  def hash(self):
+def hash(file):
     hasher = hashlib.md5()
-    with open('gallary/' + self.file, 'rb') as afile:
-      buf = afile.read()
-      hasher.update(buf)
-    self.hash = hasher.hexdigest()
-    self.jpeg = 'jpeg/' + self.hash + '.jpeg'
-    self.webp = 'webp/' + self.hash + '.webp'
-    self.jpeg_th = 'jpeg/' + self.hash + '.th.jpeg'
-    self.webp_th = 'webp/' + self.hash + '.th.webp'
+    with open('gallary/' + file, 'rb') as afile:
+        buf = afile.read()
+        hasher.update(buf)
+    hash = hasher.hexdigest()
+    return {
+        'hash':hash,
+        "jpeg": 'jpeg/' + hash + '.jpeg',
+        "webp": 'webp/' + hash + '.webp',
+        "jpeg_th": 'jpeg/' + hash + '.th.jpeg',
+        "webp_th": 'webp/' + hash + '.th.webp'
+    }
 
-  def optimize(self):
-    im = Image.open('gallary/' + self.file).convert('RGB')
-    im.save(self.jpeg, 'JPEG') # todo: TinyPNG API
-    im.save(self.webp, 'WEBP')
-    im.thumbnail((450, 300))
-    im.save(self.jpeg_th, 'JPEG')  # todo: TinyPNG API
-    im.save(self.webp_th, 'WEBP')
-
-  def manifest(self):
-      self.mani[self.hash]={
-        'source': self.file,
-        'jpeg': [self.jpeg, self.jpeg_th],
-        'webp': [self.webp, self.webp_th]
-      }
-
-  def main(self):
-    self.hash()
-    # if os.path.exists(self.jpeg) and os.path.exists(self.webp):
-    self.optimize()
-    self.manifest()
-    return self.mani
-
-def gen_manifest_json():
-  onlyfiles = [f for f in os.listdir('gallary') if os.path.isfile(os.path.join('gallary', f))]
-  id = 1
-  Manifest = {}
-  Manifest_mobile = {}
-  for f in onlyfiles:
+def process_image(file):
     try:
-      im = Image.open('gallary/' + f).convert('RGB')
-      width,height = im.size
-      if width > height:
-        worker = Single(f, Manifest)
-        Manifest = worker.main()
-      else:
-        worker = Single(f, Manifest_mobile)
-        Manifest_mobile = worker.main()
-      print(str(id) + '/' + str(len(onlyfiles)))
-      id += 1
-    except OSError:
-      print("Falied to optimize the picture: " + f)
-  with open('manifest.json', 'w+') as json_file:
-    json.dump(Manifest, json_file)
-  with open('manifest_mobile.json', 'w+') as json_file:
-    json.dump(Manifest_mobile, json_file)
+        im = Image.open('gallary/' + file).convert('RGB')
+    except Exception as e:
+        print(f'error ： {e}')
+        return False
+    width, height = im.size
+    lista = hash(file)
+    ua = "pc" if width > height else "mobile"
+    im.save(lista['jpeg'], 'JPEG', progressive=True)
+    im.save(lista['webp'], 'WEBP')
+    im.thumbnail((450, 300))
+    im.save(lista['jpeg_th'], 'JPEG', progressive=True)
+    im.save(lista['webp_th'], 'WEBP')
+    if ua == "mobile":
+        mobile[lista['hash']] = {
+            'source': file,
+            'jpeg': [lista['jpeg'], lista['jpeg_th']],
+            'webp': [lista['webp'], lista['webp_th']]
+        }
+    else:
+        pc[lista['hash']] = {
+            'source': file,
+            'jpeg': [lista['jpeg'], lista['jpeg_th']],
+            'webp': [lista['webp'], lista['webp_th']]
+        }
+    return [pc,mobile]
 
-
-def main():
-  gen_manifest_json()
+pc = {}
+mobile = {}
+def try_process():
+    filenames = [f for f in os.listdir('gallary') if os.path.isfile(os.path.join('gallary', f))]
+    Trueid = 0
+    Falseid = 0
+    errorfile = []
+    with futures.ProcessPoolExecutor(max_workers=8) as executor:
+        for result in zip(filenames, executor.map(process_image, filenames)):
+            if result[1] != False:
+                listPC=result[1][0]
+                listMobile=result[1][1]
+                pc.update(listPC)
+                mobile.update(listMobile)
+                Trueid += 1
+                print(f"已完成{Trueid}/{len(filenames)}")
+            else:
+                Falseid += 1
+                print(f"出现错误文件:{result[0]}")
+                errorfile.append(str(result[0]))
+    with open('manifest.json', 'w+') as json_file:
+        json.dump(pc, json_file)
+    with open('manifest_mobile.json', 'w+') as json_file:
+        json.dump(mobile, json_file)
+    print(f"""
+        任务已完成
+        总计数量：{len(filenames)}
+        成功处理数量：{Trueid}
+        失败数量：{Falseid}
+        失败文件：{errorfile}
+        """)
 
 
 if __name__ == '__main__':
-  main()
-  key = input('`manifest.json` saved. Press any key to quit.')
-  quit()
+    try_process()
