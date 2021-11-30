16551c9ac83cd55759f63ebaee09d50f87ae1848
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-11-30 16:44:54 +0800

    deprecate: wp_title()

--- header.php
+++ header.php
@@ -35,13 +35,6 @@ $mashiro_logo = iro_opt('mashiro_logo');
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
 	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
-	<title itemprop="name"><?php global $page, $paged;
-							wp_title('-', true, 'right');
-							bloginfo('name');
-							$site_description = get_bloginfo('description', 'display');
-							if ($site_description && (is_home() || is_front_page())) echo " - $site_description";
-							if ($paged >= 2 || $page >= 2) echo ' - ' . sprintf(__('page %s ', 'sakurairo'), max($paged, $page));/*第 %s 页*/ ?>
-	</title>
 	<?php
 	if (iro_opt('iro_meta') == true) {
 		$keywords = '';
