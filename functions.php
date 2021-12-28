37d492b872ebff50b344049306dd776890669365
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-12-28 14:46:23 +0800

    fix: disable_emojis()与 terrylinooo/githuber-md 冲突

--- functions.php
+++ functions.php
@@ -134,21 +134,24 @@ if (!function_exists('akina_setup')) :
         }
         add_action('init', 'coolwp_remove_open_sans_from_wp_core');
 
-        /**
-         * Disable the emoji's
-         */
-        function disable_emojis()
-        {
-            remove_action('wp_head', 'print_emoji_detection_script', 7);
-            remove_action('admin_print_scripts', 'print_emoji_detection_script');
-            remove_action('wp_print_styles', 'print_emoji_styles');
-            remove_action('admin_print_styles', 'print_emoji_styles');
-            remove_filter('the_content_feed', 'wp_staticize_emoji');
-            remove_filter('comment_text_rss', 'wp_staticize_emoji');
-            remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
-            add_filter('tiny_mce_plugins', 'disable_emojis_tinymce');
+        if (!function_exists('disable_emojis')) {
+            /**
+             * Disable the emoji's
+             * @see https://wordpress.org/plugins/disable-emojis/
+             */
+            function disable_emojis()
+            {
+                remove_action('wp_head', 'print_emoji_detection_script', 7);
+                remove_action('admin_print_scripts', 'print_emoji_detection_script');
+                remove_action('wp_print_styles', 'print_emoji_styles');
+                remove_action('admin_print_styles', 'print_emoji_styles');
+                remove_filter('the_content_feed', 'wp_staticize_emoji');
+                remove_filter('comment_text_rss', 'wp_staticize_emoji');
+                remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+                add_filter('tiny_mce_plugins', 'disable_emojis_tinymce');
+            }
+            add_action('init', 'disable_emojis');
         }
-        add_action('init', 'disable_emojis');
 
         /**
          * Filter function used to remove the tinymce emoji plugin.
