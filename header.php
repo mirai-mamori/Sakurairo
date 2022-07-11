fd40a5221edfa20a8d514ff5eaf5689b5f5f660b
秋尘 <78602461+Molyleaf@users.noreply.github.com>
2022-07-11 15:48:02 +0800

    回滚更改

--- header.php
+++ header.php
@@ -136,8 +136,8 @@ $mashiro_logo = iro_opt('mashiro_logo');
 			    }
 			</style>
 				<div class="dropdown">
-					<span class="menu-button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><defs><style>.fa-secondary{opacity:.4}</style></defs><path class="fa-primary" d="M416 128H32C14.33 128 0 113.7 0 96C0 78.33 14.33 64 32 64H416C433.7 64 448 78.33 448 96C448 113.7 433.7 128 416 128zM416 448H32C14.33 448 0 433.7 0 416C0 398.3 14.33 384 32 384H416C433.7 384 448 398.3 448 416C448 433.7 433.7 448 416 448z"/><path class="fa-secondary" d="M0 256C0 238.3 14.33 224 32 224H416C433.7 224 448 238.3 448 256C448 273.7 433.7 288 416 288H32C14.33 288 0 273.7 0 256z"/></svg></span>
-						<div class="dropdown-content">
+					<span class="menu-button"><i class="fa fa-bars fa-xl" style="--fa-primary-color: #ffffff; --fa-secondary-color: #c0e7ff;"></i></span>
+					<div class="dropdown-content">
 							<nav><?php wp_nav_menu(array('depth' => 2, 'theme_location' => 'primary', 'container' => false)); ?></nav>
 						</div>
 				</div>
