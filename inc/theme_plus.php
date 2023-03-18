1938664f016f5712e7e875794bb4f3ce727cf384
KotoriK <52659125+KotoriK@users.noreply.github.com>
2023-03-18 17:38:21 +0800

    fix: cannot require file

--- inc/theme_plus.php
+++ inc/theme_plus.php
@@ -847,5 +847,5 @@ function mobile_get_useragent_icon(string $ua):string{
 /**
  * chatgpt excerpt
  */
-require_once './chatgpt/hooks.php';
+require_once __DIR__.'./chatgpt/hooks.php';
 IROChatGPT\apply_chatgpt_hook();
