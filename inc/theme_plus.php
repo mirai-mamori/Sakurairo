61b9f9cf04b187cccd0e6e089aad44a459e00b2f
染川 瞳 <61381142+mirai-mamori@users.noreply.github.com>
2023-04-08 21:39:20 +0800

    Daily Update

--- inc/theme_plus.php
+++ inc/theme_plus.php
@@ -416,7 +416,7 @@ function header_user_menu()
     $ava = iro_opt('personal_avatar') ? iro_opt('personal_avatar') : get_avatar_url($current_user->user_email);
   ?>
     <div class="header-user-avatar">
-      <img class="faa-spin animated-hover" src="<?php echo get_avatar_url($current_user->ID, 64);/*$ava;*/ ?>" width="30" height="30">
+      <img src="<?php echo get_avatar_url($current_user->ID, 64);/*$ava;*/ ?>" width="30" height="30">
       <div class="header-user-menu">
         <div class="header-user-name">
           <?php _e("Signed in as", "sakurairo") ?>
