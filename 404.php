94da77ad21e09dfcaf8906b64fdbb5fde2cb7bc8
染川 瞳 <61381142+mirai-mamori@users.noreply.github.com>
2023-04-08 10:35:11 +0800

    Daily Update

--- 404.php
+++ 404.php
@@ -22,9 +22,8 @@ add_action('wp_head',function(){
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="<?php echo iro_opt('favicon_link', ''); ?>" />
     <link rel="stylesheet" href="<?= $shared_lib_basepath ?>/css/fa4.css" type="text/css" media="all"/>
-    <link rel="stylesheet" href="<?= $shared_lib_basepath ?>/css/404_lib.css" type="text/css" media="all"/>
+    <link rel="stylesheet" href="<?= $shared_lib_basepath ?>/css/404.css" type="text/css" media="all"/>
     <link rel="stylesheet" href="<?= $shared_lib_basepath ?>/css/fa6.css" type="text/css" media="all"/>
-    <link rel="stylesheet" href="<?= $shared_lib_basepath ?>/css/aplayer.css" type="text/css" media="all"/>
     <?php wp_head(); ?>
     <?php
     /* <script>
