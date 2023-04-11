bc429efe2bd24ddf41e99efe9e44d1d8d0ea1082
染川 瞳 <61381142+mirai-mamori@users.noreply.github.com>
2023-04-11 20:36:04 +0800

    Daily Update

--- inc/decorate.php
+++ inc/decorate.php
@@ -630,7 +630,7 @@ position:relative;bottom:0;left:0;display:block;width:100%;height:2px;background
 
 /*首页封面动画*/
 <?php if (iro_opt('cover_animation', 'true')): ?>
-h1.main-title, h1.fes-title,.the-feature.from_left_and_right .info,.header-info p,.header-info,.header-shuo,.focusinfo .header-tou img,.top-social img,.center-text{
+h1.main-title, h1.fes-title,.the-feature.from_left_and_right .info,.header-info p,.header-info,.header-shuo,.header-shuo p,.focusinfo .header-tou img,.top-social img,.center-text{
 	-moz-animation: fadeInDown  <?=iro_opt('cover_animation_time'); ?>s;
     -webkit-animation:fadeInDown  <?=iro_opt('cover_animation_time'); ?>s;
 	animation: fadeInDown  <?=iro_opt('cover_animation_time'); ?>s;
