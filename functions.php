24e1c682c8eff035330d69ffee1ebb7d2993d7c7
染川 瞳 <61381142+mirai-mamori@users.noreply.github.com>
2023-04-12 23:06:00 +0800

    Daily Update

--- functions.php
+++ functions.php
@@ -1823,6 +1823,30 @@ function permalink_tip()
 add_action('admin_notices', 'permalink_tip');
 //code end
 
+//发送主题版本号 
+function send_theme_version() {
+    $theme = wp_get_theme();
+    $version = $theme->get('Version');
+    $data = array(
+        'date' => date('Y-m-d H:i:s'),
+        'version' => $version
+    );
+    $args = array(
+        'body' => $data,
+        'timeout' => '5',
+        'redirection' => '5',
+        'httpversion' => '1.0',
+        'blocking' => true,
+        'headers' => array(),
+        'cookies' => array()
+    );
+    wp_remote_post('https://api.maho.cc/ver-stat/index.php', $args);
+}
+if (!wp_next_scheduled('my_hourly_event')) {
+    wp_schedule_event(time(), 'hourly', 'my_hourly_event');
+}
+add_action('my_hourly_event', 'send_theme_version');
+
 //解析短代码  
 add_shortcode('task', 'task_shortcode');
 function task_shortcode($attr, $content = '')
