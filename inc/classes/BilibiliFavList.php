a3d87d22eead8c0d7e5f2a75007a6fab5d217386
KotoriK <52659125+KotoriK@users.noreply.github.com>
2023-04-15 16:00:41 +0800

    fix(Bilibili收藏模板): 修复了未使用cookie进行请求的问题；提供了更好的报错提示
    Fix #683
    
    add cookie to headers

--- inc/classes/BilibiliFavList.php
+++ inc/classes/BilibiliFavList.php
@@ -5,10 +5,15 @@ namespace Sakura\API;
 class BilibiliFavList
 {
 	private $uid;
-
+	private $headers;
 	public function __construct()
 	{
 		$this->uid = iro_opt('bilibili_id');
+		$this->headers = array(
+			'Host' => 'api.bilibili.com',
+			'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97',
+			"Cookie" => iro_opt('bilibili_cookie')
+		);
 	}
 
 	function fetch_folder_api()
@@ -16,32 +21,29 @@ class BilibiliFavList
 		$uid = $this->uid;
 		$url = "https://api.bilibili.com/x/v3/fav/folder/created/list-all?up_mid=$uid&jsonp=jsonp";
 		$args = array(
-			'headers' => array(
-				'Host' => 'api.bilibili.com',
-				'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97'
-			)
+			'headers' => $this->headers
 		);
 		$response = wp_remote_get($url, $args);
-		if(is_array($response)){
-			return json_decode($response["body"], true) || false;
-		}else{
-			return false;
+		if (is_array($response)) {
+			$result = json_decode($response["body"], true);
+			if ($result) {
+				return $result;
+			}
 		}
+		error_log("BilibiliFavList: fetch_folder_api failed, response dump:" . var_export($response, true));
+		return false;
 	}
 
 	function fetch_folder_item_api(int $folder_id, int $page)
 	{
 		$url = "https://api.bilibili.com/x/v3/fav/resource/list?media_id=$folder_id&pn=$page&ps=9&platform=web&jsonp=jsonp";
 		$args = array(
-			'headers' => array(
-				'Host' => 'api.bilibili.com',
-				'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97'
-			)
+			'headers' => $this->headers
 		);
 		$response = wp_remote_get($url, $args);
-		if(is_array($response)){
+		if (is_array($response)) {
 			return json_decode($response["body"], true) || false;
-		}else{
+		} else {
 			return false;
 		}
 	}
@@ -49,15 +51,18 @@ class BilibiliFavList
 	public function get_folders()
 	{
 		$resp = $this->fetch_folder_api();
-		if ($resp === false)
-		{
+		if ($resp === false) {
 			return "<div>" . __('Backend error', 'sakurairo') . "</div>";
 		}
 		$folders_data = $resp['data'];
+		if ($folders_data === null) {
+			error_log("BilibiliFavList: no data fetched. Try apply cookie.");
+			return "<div>" . __('Backend error', 'sakurairo') . "</div>";
+		}
 		$folders = $folders_data['list'];
 		$html = '';
-		foreach ((array)$folders as $folder){
-			$html .= BilibiliFavList::folder_display($folder['id']);
+		foreach ((array)$folders as $folder) {
+			$html .= $this->folder_display($folder['id']);
 		}
 		return $html;
 	}
@@ -66,12 +71,12 @@ class BilibiliFavList
 	{
 		$folder_resp = $this->fetch_folder_item_api($folder_id, 1);
 		$folder_content_info = $folder_resp['data']['info'];
-		$html = '<div class="folder"><div class="folder-top">'.
-		        lazyload_img(str_replace('http://', 'https://', $folder_content_info['cover']),'folder-img',array('alt'=>$folder_content_info['title'])).
-		        '<div class="folder-detail"><h3>' . $folder_content_info['title'] . '</h3>'.
-		        '<p>' . __('Item count: ', 'sakurairo') . $folder_content_info['media_count'] . '</p>'.
-		        '<button class="expand-button">' . __('Expand', 'sakurairo') . '</button></div></div>'.
-		        '<hr><div class="folder-content">';
+		$html = '<div class="folder"><div class="folder-top">' .
+			lazyload_img(str_replace('http://', 'https://', $folder_content_info['cover']), 'folder-img', array('alt' => $folder_content_info['title'])) .
+			'<div class="folder-detail"><h3>' . $folder_content_info['title'] . '</h3>' .
+			'<p>' . __('Item count: ', 'sakurairo') . $folder_content_info['media_count'] . '</p>' .
+			'<button class="expand-button">' . __('Expand', 'sakurairo') . '</button></div></div>' .
+			'<hr><div class="folder-content">';
 		$load = BilibiliFavList::load_more(rest_url('sakura/v1/favlist/bilibili') . '?page=1' . '&folder_id=' . $folder_id);
 		return $html . $load . '</div></div></br>';
 	}
@@ -84,9 +89,9 @@ class BilibiliFavList
 		foreach ((array)$folder_content as $item) {
 			$html .= BilibiliFavList::folder_item_display($item);
 		}
-		if ($folder_resp['data']['has_more']){
+		if ($folder_resp['data']['has_more']) {
 			$load = BilibiliFavList::load_more(rest_url('sakura/v1/favlist/bilibili') . '?page=' . ++$page . '&folder_id=' . $folder_id);
-		}else{
+		} else {
 			$load = '<a class="load-more"><i class="fa-solid fa-ban"></i>' . __('All item has been loaded.', 'sakurairo') . '</a>';
 		}
 		return $html . $load;
@@ -94,17 +99,17 @@ class BilibiliFavList
 
 	private static function folder_item_display(array $item)
 	{
-		if ($item['type'] == 24){
+		if ($item['type'] == 24) {
 			$link = $item['link'];
-		}else{
+		} else {
 			$link = "https://www.bilibili.com/video/" . $item['bvid'];
 		}
 		// TODO: add lazyload to item-image with typescript
-		return '<div class="column"><a class="folder-item" href="' . $link . '" target="_blank" rel="nofollow">'.
-		       '<img class="item-image" src="' . $item['cover'] . '">'.
-		       '<div class="item-info"><h3 class="item-title" title="' . $item['title'] . '">' . $item['title'] . '</h3>'.
-		       '<div class="item-intro" title="' . $item['intro'] . '">' . $item['intro'] . '</div>'.
-		       '</div></a></div>';
+		return '<div class="column"><a class="folder-item" href="' . $link . '" target="_blank" rel="nofollow">' .
+			'<img class="item-image" src="' . $item['cover'] . '">' .
+			'<div class="item-info"><h3 class="item-title" title="' . $item['title'] . '">' . $item['title'] . '</h3>' .
+			'<div class="item-intro" title="' . $item['intro'] . '">' . $item['intro'] . '</div>' .
+			'</div></a></div>';
 	}
 
 	private static function load_more($href)
