7063a3606cfb9e0e99f0febb3a0c449557c3798f
KanoChan <helloworld@kanochan.net>
2023-05-15 00:15:33 +0800

    修复：文章为空时check_title_tags()函数执行异常（关联友链模板）

--- page-links.php
+++ page-links.php
@@ -8,21 +8,21 @@ get_header();
 
 ?>
 	<?php while(have_posts()) : the_post(); ?>
+	<?php $post = get_post(); ?>
 	<?php if(!iro_opt('patternimg') || !get_post_thumbnail_id(get_the_ID())) { ?>
-	<span class="linkss-title"><?php the_title();?></span>
+		<span class="linkss-title"><?php the_title();?></span>
 	<?php } ?>
 		<article <?php post_class("post-item"); ?>>
+			<?php if (iro_opt('article_auto_toc', 'true') && check_title_tags($post->post_content)) { //加载目录?>
+			<div class="has-toc have-toc"></div>
+			<?php } ?>
 			<div class="entry-content">
 				<?php the_content( '', true ); ?>
-			</div>
+			</div>			
 			<div class="links">
 				<?php echo get_link_items(); ?>
 			</div>
 		</article>
-	<?php
-		$post = get_post();
-		if (iro_opt('article_auto_toc', 'true') && (check_title_tags($post->post_content))) echo '<div class="has-toc have-toc"></div>';
-	?>
 	<?php get_template_part('layouts/sidebox'); //加载目录容器?> 
 	<?php endwhile; ?>
 <?php
