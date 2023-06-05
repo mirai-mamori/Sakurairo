eec65045e528383b0b36d53de92fdd2cb6fea35c
染川 瞳 <61381142+mirai-mamori@users.noreply.github.com>
2023-06-05 14:12:35 +0800

    PUC Up to 5.1

--- update-checker/update-checker.php
+++ update-checker/update-checker.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Plugin Update Checker Library 5.0
+ * Plugin Update Checker Library 5.1
  * http://w-shadow.com/
  *
- * Copyright 2022 Janis Elsts
+ * Copyright 2023 Janis Elsts
  * Released under the MIT license. See license.txt for details.
  */
 
-require dirname(__FILE__) . '/load-v5p0.php';
+require dirname(__FILE__) . '/load-v5p1.php';
