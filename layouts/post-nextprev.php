5a39370d027db20a41e8c8cd9077b9eb50f2bda2
KanoChan <helloworld@kanochan.net>
2023-06-18 05:24:07 +0800

    fix(post-nextprev): “上/下一篇文章”区域样式错误
    错误的样式：https://github.com/mirai-mamori/Sakurairo/assets/79365334/12baafe6-d80c-4bb7-84f3-677ef66a050c
    
    get_next_post()或get_previous_post()可选的参数见：https://developer.wordpress.org/reference/functions/get_next_post/
    
    get_next_post($excluded_terms = $classify_display_id)缺少了“in_same_term”参数，如果$classify_display_id的值不为空，get_next_post()会将“$excluded_terms”的值作为“in_same_term”，即”$in_same_term  =  true”。
    这会导致get_next_post()函数只会在同分类中检索文章，且没有排除已设置的分类。
    
    get_previous_post()函数同上。

--- layouts/post-nextprev.php
+++ layouts/post-nextprev.php
@@ -8,10 +8,10 @@ if (iro_opt('article_nextpre') == '1') {
 ?>
 <section class="post-squares nextprev">
 	<?php iro_opt('classify_display') ? $classify_display_id = iro_opt('classify_display') : $classify_display_id = null; ?>
-	<div class="post-nepre <?php if(get_next_post($excluded_terms = $classify_display_id)){echo 'half';}else{echo 'full';} ?> previous">
+	<div class="post-nepre <?php if(get_next_post(false, $classify_display_id)){echo 'half';}else{echo 'full';} ?> previous">
 		<?php previous_post_link( $format = '%link', $link = '<div class="background lazyload" style="background-image:url('.iro_opt('vision_resource_basepath','https://s.nmxc.ltd/sakurairo_vision/@2.6/').'load_svg/outload.svg);" data-src="'.get_prev_thumbnail_url().'"></div><span class="label">'.__("Previous Post",'sakurairo').'</span><div class="info"><h3>%title</h3><hr></div>', $in_same_term = false, $excluded_terms = $classify_display_id ) ?>
 	</div>
-	<div class="post-nepre <?php if(get_previous_post($excluded_terms = $classify_display_id)){echo 'half';}else{echo 'full';} ?> next">
+	<div class="post-nepre <?php if(get_previous_post(false, $classify_display_id)){echo 'half';}else{echo 'full';} ?> next">
 		<?php next_post_link( $format = '%link', $link = '<div class="background lazyload" style="background-image:url('.iro_opt('vision_resource_basepath','https://s.nmxc.ltd/sakurairo_vision/@2.6/').'load_svg/outload.svg);" data-src="'.get_next_thumbnail_url().'"></div><span class="label">'.__("Next Post",'sakurairo').'</span><div class="info"><h3>%title</h3><hr></div>', $in_same_term = false, $excluded_terms = $classify_display_id ) ?>
 	</div>
 </section>
