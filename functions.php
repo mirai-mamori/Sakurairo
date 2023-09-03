07516d65676314e62b94a93e8e22a9940b6fa336
染川 瞳 <61381142+mirai-mamori@users.noreply.github.com>
2023-09-03 13:04:39 +0800

    Daily Update

--- functions.php
+++ functions.php
@@ -2031,7 +2031,7 @@ add_shortcode('ghcard', 'gh_card');
 function gh_card($attr, $content = '')
 {
     extract(shortcode_atts(array("path" => ""), $attr));
-    return '<div class="ghcard"><a href="https://github.com/'. $path .'"><img src="https://github-readme-stats.maho.cc/api'. $content .'" alt="Github-Card"></a></div>';
+    return '<div class="ghcard"><a href="https://github.com/'. $path .'"><img src="http://github-profile-summary-cards.vercel.app/api'. $content .'" alt="Github-Card"></a></div>';
 }
 
 add_shortcode('showcard', 'show_card');
