54683d8834a52ad380ae12fea6c5d9fe23cd9ab5
染川 瞳 <61381142+mirai-mamori@users.noreply.github.com>
2024-01-06 11:54:29 +0800

    Version 2.?.?

--- footer.php
+++ footer.php
@@ -118,7 +118,7 @@ $reception_background = iro_opt('reception_background');
   <?php if (iro_opt('widget_daynight', 'true')): ?>
     <ul class="menu-list">
       <li id="white-bg" title="<?=__('Light Mode','sakurairo');?>" >
-        <i class="fa-solid fa-display fa-sm"></i>
+        <i class="fa-regular fa-sun"></i>
       </li><!--Default-->
       <li id="dark-bg" title="<?=__('Dark Mode','sakurairo');?>" >
         <i class="fa-regular fa-moon"></i>
--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
 
 
 define('IRO_VERSION', wp_get_theme()->get('Version'));
-define('INT_VERSION', '18.3.1');
+define('INT_VERSION', '18.4.0');
 define('BUILD_VERSION', '2');
 
 function check_php_version($preset_version) {
