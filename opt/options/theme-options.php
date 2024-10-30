e1fc033cfd407c88424e19f06cc892fdb9a948d1
nicocatxzc <114812330+nicocatxzc@users.noreply.github.com>
2024-10-31 00:01:18 +0800

    Update theme-options.php
    修复之前使用自定义api但是服务器指向custom

--- opt/options/theme-options.php
+++ opt/options/theme-options.php
@@ -844,17 +844,16 @@ $prefix = 'iro_options';
           'netease' => __('Netease Cloud Music','sakurairo_csf'),
           'kugou' => __('Kugou Music(may not be available)','sakurairo_csf'),
           'tencent' => __('QQ Music(may not be available)','sakurairo_csf'),
-          'custom' => __('custom server','sakurairo_csf'),
         ),
         'default' => 'off'
       ),
 
       array(
-        'id' => 'custom_server',
+        'id' => 'custom_music_api',
         'type' => 'text',
-        'title' => __('Custom playlist','sakurairo_csf'),
-        'dependency' => array( 'aplayer_server', '==', 'custom', '', 'true' ),
-        'desc' => __('Use custom playlist or meting-api','sakurairo_csf'),
+        'title' => __('自定义歌单','sakurairo_csf'),
+        'dependency' => array( 'aplayer_server', '!=', 'off', '', 'true' ),
+        'desc' => __('使用自定义meting-api，也可以指向歌单文件，但只有填了歌单id才生效','sakurairo_csf'),
       ),
 
       array(
