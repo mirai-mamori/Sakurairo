60d2cc0f22aa0165468e2c9aaf4f76dd4f11420b
nicocatxzc <114812330+nicocatxzc@users.noreply.github.com>
2024-11-04 14:02:59 +0800

    将主页是否显示说说判断做成变量，减少对数据库的查询

--- tpl/content-thumb.php
+++ tpl/content-thumb.php
@@ -16,20 +16,19 @@ $is_author_page = is_author() && !is_home() && !is_category() && !is_tag();
 
 $sticky_posts = get_option('sticky_posts');
 
+$show_shuoshuo_on_home = is_home() && iro_opt('show_shuoshuo_on_home_page');
+
 // Query for sticky posts (only on the first page)
 if ($paged == 1 && !empty($sticky_posts)) {
     $sticky_args = array(
-        'post_type' => array('post', 'shuoshuo'),
+        'post_type' => $show_shuoshuo_on_home ? array('post', 'shuoshuo') : array('post'),
         'post_status' => 'publish',
         'posts_per_page' => -1, // Get all sticky posts
         'post__in' => $sticky_posts,
         'orderby' => 'post_date',
         'order' => 'DESC',
     );
-	if (is_home() && !iro_opt('show_shuoshuo_on_home_page')) {
-		//是否在主页显示说说
-		$sticky_args['post_type'] = array('post');
-	}
+	
     if ($is_author_page) {
         $sticky_args['author'] = get_the_author_meta('ID');
     }
@@ -46,7 +45,7 @@ if ($paged == 1 && !empty($sticky_posts)) {
 
 // Query for non-sticky posts
 $non_sticky_args = array(
-    'post_type' => array('post', 'shuoshuo'),
+    'post_type' => $show_shuoshuo_on_home ? array('post', 'shuoshuo') : array('post'),
     'post_status' => 'publish',
     'posts_per_page' => 10, // 每页显示10篇文章
     'orderby' => 'post_date',
@@ -55,10 +54,7 @@ $non_sticky_args = array(
     'post__not_in' => $sticky_posts,
     'ignore_sticky_posts' => 1
 );
-if (is_home() && !iro_opt('show_shuoshuo_on_home_page')) {
-	//是否在主页显示说说
-	$non_sticky_args['post_type'] = array('post');
-}
+
 if ($is_author_page) {
     $non_sticky_args['author'] = get_the_author_meta('ID'); // 只获取当前作者的文章
 }
