f5fa0db1c47a7b0ef849dcc8a0a9fb3960347dad
染川 瞳 <me@hareru.org>
2024-12-07 16:19:37 +0900

    Daily Update

--- inc/classes/Bilibili.php
+++ inc/classes/Bilibili.php
@@ -95,7 +95,7 @@ class Bilibili
     }
     private static function anchor_pagination_next(string $href)
     {
-        return '<a class="bangumi-next" data-href="' . $href . '"><i class="fa-solid fa-bolt-lightning"></i></i> NEXT </a>';
+        return '<a class="bangumi-next" data-href="' . $href . '"><i class="fa-solid fa-bolt-lightning"></i>' . __('Load More', 'sakurairo') . '</a>';
     }
     private static function bangumi_item(array $item, $percent)
     {
