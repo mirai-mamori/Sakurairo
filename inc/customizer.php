44b542ebf7b54e4fe602315a06637dea851171f4
Leaphy <69081090+leaphy-dev@users.noreply.github.com>
2025-01-01 03:57:07 +0800

    更改wp自带主题设置中关于静态页面的描述 customizer.php

--- inc/customizer.php
+++ inc/customizer.php
@@ -24,3 +24,13 @@ function akina_customize_preview_js() {
 	wp_enqueue_script( 'akina_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', 'akina_customize_preview_js' );
+
+
+function add_homepage_controls_description($wp_customize) {
+    // 移除静态主页选择器
+    $wp_customize->remove_control('page_on_front');
+    // 为静态页面选择器添加描述
+    $wp_customize->get_control('page_for_posts')->description = '选择取代文章列表显示的页面。此页面的标题将替代文章区域标题，其内容将替代文章列表。';
+}
+add_action('customize_register', 'add_homepage_controls_description');
+
