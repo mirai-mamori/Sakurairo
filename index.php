daee5ea260a8b77cdbc8e11aef6c21a21488b034
Leaphy <leaf443@outlook.com>
2025-02-01 09:50:48 +0800

    '_'开头的静态页面不显示标题

--- index.php
+++ index.php
@@ -39,7 +39,11 @@ foreach ($component_order as $component) {
                 ?>
                 <section class="custom-static-section">
                     <h1 class="main-title static-page-title">
-                        <?php echo esc_html(get_the_title($static_page)); ?>
+                        <?php if (!strpos(get_the_title($static_page), "_")===0){
+                            # '_'开头的静态页面不显示标题
+                         echo esc_html(get_the_title($static_page));
+                        }
+                         ?>
                     </h1>
                     <div class="static-page-content">
                         <?php echo apply_filters('the_content', $static_page->post_content); ?>
