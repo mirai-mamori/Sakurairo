71794cc384dc17f52b063a6bfc87f1185696a5a7
nicocatxzc <nicocatxzc@163.com>
2025-02-22 20:09:53 +0800

    移除函数

--- inc/theme_plus.php
+++ inc/theme_plus.php
@@ -459,48 +459,6 @@ function header_user_menu()
   }
 }
 
-/**
- * 移动端侧边栏用户菜单
- *
- * @return void
- */
-function m_user_menu()
-{
-  global $current_user;
-  wp_get_current_user();
-  if (is_user_logged_in()) {?>
-    <div class="m-user-menu">
-      <div class="m-user-name">
-        <span><?php echo $current_user->display_name; ?></span>
-      </div>
-      <div class="m-user-menu-option">
-        <?php if (current_user_can('manage_options')) { ?>
-          <a href="<?php bloginfo('url'); ?>/wp-admin/" target="_blank"><?php _e('Dashboard', 'sakurairo')/*管理中心*/ ?></a>
-          <a href="<?php bloginfo('url'); ?>/wp-admin/post-new.php" target="_blank"><?php _e('New post', 'sakurairo')/*撰写文章*/ ?></a>
-        <?php } ?>
-        <a href="<?php bloginfo('url'); ?>/wp-admin/profile.php" target="_blank"><?php _e('Profile', 'sakurairo')/*个人资料*/ ?></a>
-        <a href="<?php echo wp_logout_url(get_bloginfo('url')); ?>" target="_top" data-no-pjax><?php _e('Sign out', 'sakurairo')/*退出登录*/ ?></a>
-      </div>
-    </div>
-  <?php
-  } else {
-    global $wp;
-    $login_url = iro_opt('exlogin_url') ? iro_opt('exlogin_url') : wp_login_url(iro_opt('login_urlskip') ? '' : add_query_arg($wp->query_vars, home_url($wp->request)));
-  ?>
-    <div class="m-user-avatar">
-      <div class="m-user-menu">
-        <div class="m-user-name no-logged">
-          <a id="login-link" href="<?= $login_url ?>" data-no-pjax style="font-weight:bold;text-decoration:none"><?php _e('Log in', 'sakurairo')/*登录*/ ?></a>
-          <?php if (get_option('users_can_register')) { ?>
-            <a style="font-weight:bold;text-decoration:none" href="<?php echo wp_registration_url() ?>"><?php _e('Register') ?></a>
-          <?php } ?>
-        </div>
-      </div>
-    </div>
-  <?php
-  }
-}
-
 /*
  * 获取相邻文章缩略图
  * 特色图 -> 文章图 -> 首页图
