1b4aef91f2179e0025cac9bec50a9ab4dc2f5a1e
nicocatxzc <nicocatxzc@163.com>
2025-03-12 01:03:58 +0800

    移除自定义页的背景设置

--- functions.php
+++ functions.php
@@ -163,17 +163,6 @@ if (!function_exists('akina_setup')) {
             )
         );
 
-        // Set up the WordPress core custom background feature.
-        add_theme_support(
-            'custom-background',
-            apply_filters(
-                'akina_custom_background_args',
-                array(
-                    'default-color' => 'ffffff',
-                    'default-image' => '',
-                )
-            )
-        );
         /**
          * 废弃过时的wp_title
          * @seealso https://make.wordpress.org/core/2015/10/20/document-title-in-4-4/
