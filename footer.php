6d4b74e34639c3ba2eea0133e4b0e4f420ad1b8d
染川 瞳 <me@hareru.org>
2025-03-24 20:45:14 +0900

    Daily  Update

--- footer.php
+++ footer.php
@@ -166,7 +166,7 @@ $reception_background = iro_opt('reception_background');
       width: 100vw;
       transition: none;
       pointer-events: none;
-      z-index: -1;
+      z-index: 0;
     }
     .current_blur {
       -webkit-backdrop-filter: saturate(120%) blur(8px);
