2633c55cb61e7f54f6373d382aa25ce3b68404cc
染川 瞳 <me@hareru.org>
2025-05-02 17:09:21 +0900

    Daily Update

--- exhibition.php
+++ exhibition.php
@@ -2,75 +2,116 @@
 $exhibition = iro_opt('exhibition');
 $exhibition = is_array($exhibition) ? $exhibition : [];
 
-// 获取站点统计信息
-$site_stats = get_site_stats();
+// 获取组件配置
 $components = iro_opt("capsule_components",[]);
-// 确保$components始终是数组
 $components = is_array($components) ? $components : [];
 
-// 准备显示信息
-$square_cards = [
-    'post_count'       => [
-        'icon'    => 'fa-regular fa-file-lines',
-        'label'   => __('Content Count', 'sakurairo'),
-        'value'   => number_format($site_stats['post_count']) . ' ' . __('posts', 'sakurairo'),
-        'enabled' => in_array('post_count', $components)
-    ],
-    'comment_count'    => [
-        'icon'    => 'fa-regular fa-comment',
-        'label'   => __('Comment Count', 'sakurairo'),
-        'value'   => number_format($site_stats['comment_count']) . ' ' . __('comments', 'sakurairo'),
-        'enabled' => in_array('comment_count', $components)
-    ],
-    'view_count'    => [
-        'icon'    => 'fa-regular fa-eye',
-        'label'   => __('Visitor Count', 'sakurairo'),
-        'value'   => number_format($site_stats['visitor_count']) . ' ' . __('visits', 'sakurairo'),
-        'enabled' => in_array('view_count', $components)
-    ],
-    'link_count'       => [
-        'icon'    => 'fa-solid fa-link',
-        'label'   => __('Link Count', 'sakurairo'),
-        'value'   => number_format($site_stats['link_count']) . ' ' . __('links', 'sakurairo'),
-        'enabled' => in_array('link_count', $components)
-    ],
-    'author_count'     => [
-        'icon'    => 'fa-solid fa-users',
-        'label'   => __('Author Count', 'sakurairo'),
-        'value'   => number_format($site_stats['author_count']) . ' ' . __('authors', 'sakurairo'),
-        'enabled' => in_array('author_count', $components)
-    ],
-    'total_words' => [
-        'icon'    => 'fa-solid fa-font',
-        'label'   => __('Total Words', 'sakurairo'),
-        'value'   => number_format($site_stats['total_words']) . ' ' . __('words', 'sakurairo'),
-        'enabled' => in_array('total_words', $components)
-    ],
-    'blog_days'   => [
-        'icon'    => 'fa-solid fa-calendar-days',
-        'label'   => __('Blog Running', 'sakurairo'),
-        'value'   => number_format($site_stats['blog_days']) . ' ' . __('days', 'sakurairo'),
-        'enabled' => in_array('blog_days', $components)
-    ],
-    'admin_online'=> [
-        'icon'    => 'fa-solid fa-user-clock',
-        'label'   => __('Last Online', 'sakurairo'),
-        'value'   => format_time_diff($site_stats['admin_last_online_diff']),
-        'enabled' => in_array('admin_online', $components)
-    ],
-    'announcement'=> [
-        'icon'         => 'fa-solid fa-bullhorn',
-        'label'        => iro_opt('stat_announcement_text', __('Latest Announcement', 'sakurairo')),
-        'value'        => '',
-        'enabled'      => in_array('announcement', $components)
-    ],
-    'random_link'=> [
-        'enabled'      => in_array('random_link', $components),
-    ]
-];
+// 如果组件为空且没有展示内容，则提前退出
+if (empty($components) && empty($exhibition)) {
+    // 没有任何内容要显示，可以直接返回空白
+    echo '</div>'; // 关闭可能存在的父容器
+    return;
+}
 
-// 是否显示徽章胶囊功能
-$show_medal_capsules = iro_opt('show_medal_capsules', true);
+// 预定义组件数组，仅在需要时填充内容
+$square_cards = [];
+$site_stats = []; // 只在需要时获取站点统计
+
+// 检查是否至少有一个组件被启用
+$has_valid_components = !empty($components);
+
+// 仅当有组件需要显示时才获取站点统计信息
+if ($has_valid_components) {
+    $site_stats = get_site_stats();
+    
+    // 动态填充需要使用的组件
+    if (in_array('post_count', $components)) {
+        $square_cards['post_count'] = [
+            'icon'    => 'fa-regular fa-file-lines',
+            'label'   => __('Content Count', 'sakurairo'),
+            'value'   => number_format($site_stats['post_count']) . ' ' . __('posts', 'sakurairo'),
+            'enabled' => true
+        ];
+    }
+    
+    if (in_array('comment_count', $components)) {
+        $square_cards['comment_count'] = [
+            'icon'    => 'fa-regular fa-comment',
+            'label'   => __('Comment Count', 'sakurairo'),
+            'value'   => number_format($site_stats['comment_count']) . ' ' . __('comments', 'sakurairo'),
+            'enabled' => true
+        ];
+    }
+    
+    if (in_array('view_count', $components)) {
+        $square_cards['view_count'] = [
+            'icon'    => 'fa-regular fa-eye',
+            'label'   => __('Visitor Count', 'sakurairo'),
+            'value'   => number_format($site_stats['visitor_count']) . ' ' . __('visits', 'sakurairo'),
+            'enabled' => true
+        ];
+    }
+    
+    if (in_array('link_count', $components)) {
+        $square_cards['link_count'] = [
+            'icon'    => 'fa-solid fa-link',
+            'label'   => __('Link Count', 'sakurairo'),
+            'value'   => number_format($site_stats['link_count']) . ' ' . __('links', 'sakurairo'),
+            'enabled' => true
+        ];
+    }
+    
+    if (in_array('author_count', $components)) {
+        $square_cards['author_count'] = [
+            'icon'    => 'fa-solid fa-users',
+            'label'   => __('Author Count', 'sakurairo'),
+            'value'   => number_format($site_stats['author_count']) . ' ' . __('authors', 'sakurairo'),
+            'enabled' => true
+        ];
+    }
+    
+    if (in_array('total_words', $components)) {
+        $square_cards['total_words'] = [
+            'icon'    => 'fa-solid fa-font',
+            'label'   => __('Total Words', 'sakurairo'),
+            'value'   => number_format($site_stats['total_words']) . ' ' . __('words', 'sakurairo'),
+            'enabled' => true
+        ];
+    }
+    
+    if (in_array('blog_days', $components)) {
+        $square_cards['blog_days'] = [
+            'icon'    => 'fa-solid fa-calendar-days',
+            'label'   => __('Blog Running', 'sakurairo'),
+            'value'   => number_format($site_stats['blog_days']) . ' ' . __('days', 'sakurairo'),
+            'enabled' => true
+        ];
+    }
+    
+    if (in_array('admin_online', $components)) {
+        $square_cards['admin_online'] = [
+            'icon'    => 'fa-solid fa-user-clock',
+            'label'   => __('Last Online', 'sakurairo'),
+            'value'   => format_time_diff($site_stats['admin_last_online_diff']),
+            'enabled' => true
+        ];
+    }
+    
+    if (in_array('announcement', $components)) {
+        $square_cards['announcement'] = [
+            'icon'         => 'fa-solid fa-bullhorn',
+            'label'        => iro_opt('stat_announcement_text', __('Latest Announcement', 'sakurairo')),
+            'value'        => '',
+            'enabled' => true
+        ];
+    }
+    
+    if (in_array('random_link', $components)) {
+        $square_cards['random_link'] = [
+            'enabled' => true
+        ];
+    }
+}
 
 // 判断博客运行天数徽章级别
 function get_blog_days_medal($days) {
@@ -266,15 +307,36 @@ function get_words_count_medal($words) {
 
 // 统计徽章信息
 $medal_levels = [];
-if ($show_medal_capsules) {
-    $m = get_blog_days_medal($site_stats['blog_days']);
-    if ($m) $medal_levels['blog_days'] = $m;
-    $m = get_visitor_count_medal($site_stats['visitor_count']);
-    if ($m) $medal_levels['view_count'] = $m;
-    $m = get_link_count_medal($site_stats['link_count']);
-    if ($m) $medal_levels['link_count'] = $m;
-    $m = get_words_count_medal($site_stats['total_words']);
-    if ($m) $medal_levels['total_words'] = $m;
+// 只有启用了相关组件且启用了徽章胶囊功能时才计算徽章
+$show_medal_capsules = iro_opt('show_medal_capsules', true);
+$need_medals = $show_medal_capsules && (
+    in_array('blog_days', $components) ||
+    in_array('view_count', $components) ||
+    in_array('link_count', $components) ||
+    in_array('total_words', $components)
+);
+
+// 只有当需要显示徽章时才计算徽章级别
+if ($need_medals) {
+    if (in_array('blog_days', $components)) {
+        $m = get_blog_days_medal($site_stats['blog_days']);
+        if ($m) $medal_levels['blog_days'] = $m;
+    }
+    
+    if (in_array('view_count', $components)) {
+        $m = get_visitor_count_medal($site_stats['visitor_count']);
+        if ($m) $medal_levels['view_count'] = $m;
+    }
+    
+    if (in_array('link_count', $components)) {
+        $m = get_link_count_medal($site_stats['link_count']);
+        if ($m) $medal_levels['link_count'] = $m;
+    }
+    
+    if (in_array('total_words', $components)) {
+        $m = get_words_count_medal($site_stats['total_words']);
+        if ($m) $medal_levels['total_words'] = $m;
+    }
 }
 
 ?>
@@ -286,37 +348,30 @@ if ($show_medal_capsules) {
       <!-- Bento布局容器 -->
        <div class="bento-grid">
         <!-- 胶囊 -->
-         
-        <?php 
-        // Check if there are any valid components to display
-        $has_valid_components = false;
-        if (!empty($components)) {
-            foreach($components as $component) {
-                if (isset($square_cards[$component])) {
-                    $has_valid_components = true;
-                    break;
-                }
-            }
-        }
-        if($has_valid_components): 
-        ?>
+        <?php if (!empty($components) && !empty($square_cards)): ?>
         <div class="stat-capsules-container">
-            <?php foreach($components as $component) :
+            <?php 
+            // 优化：只迭代存在于square_cards中的组件
+            foreach($components as $component):
+                if (!isset($square_cards[$component])) {
+                    continue; // 跳过不存在的组件
+                }
+                
                 switch ($component):
                     case 'post_count':
                     case 'comment_count':
                     case 'author_count':
                     case 'admin_online':
-                        if ($square_cards[$component]['enabled']): // 理论上选项中不添加相关组件后就不会case到，因为没有?>
-                            <div class="stat-capsule">
-                              <i class="<?php echo $square_cards[$component]['icon']; ?>"></i>
-                              <div class="capsule-content">
-                                <span class="capsule-label"><?php echo $square_cards[$component]['label']; ?></span>
-                                <span class="capsule-value"><?php echo $square_cards[$component]['value']; ?></span>
-                              </div>
-                            </div>
-                          <?php endif;
-                          break;
+                        ?>
+                        <div class="stat-capsule">
+                          <i class="<?php echo $square_cards[$component]['icon']; ?>"></i>
+                          <div class="capsule-content">
+                            <span class="capsule-label"><?php echo $square_cards[$component]['label']; ?></span>
+                            <span class="capsule-value"><?php echo $square_cards[$component]['value']; ?></span>
+                          </div>
+                        </div>
+                        <?php
+                        break;
 
                     case 'view_count':
                     case 'link_count':
@@ -344,10 +399,7 @@ if ($show_medal_capsules) {
                                 <div class="capsule-content">
                                     <span class="capsule-label"><?php echo $medal['label']; ?></span>
                                     <span class="capsule-value">
-                                    <?php 
-                                        // 徽章下依旧显示对应的数值
-                                        echo $square_cards[$component]['value']; 
-                                    ?>
+                                    <?php echo $square_cards[$component]['value']; ?>
                                     </span>
                                 </div>
                                 <div class="medal-info-tooltip">
@@ -361,8 +413,7 @@ if ($show_medal_capsules) {
                                     <?php endif; ?>
                                 </div>
                             </div>
-                        <?php else: 
-                            if ($square_cards[$component]['enabled']): ?>
+                        <?php else: ?>
                             <div class="stat-capsule">
                                 <i class="<?php echo $square_cards[$component]['icon']; ?>"></i>
                                 <div class="capsule-content">
@@ -370,15 +421,14 @@ if ($show_medal_capsules) {
                                 <span class="capsule-value"><?php echo $square_cards[$component]['value']; ?></span>
                                 </div>
                             </div>
-                        <?php   endif;
-                        endif;
+                        <?php endif;
                         break;
 
                     case 'random_link':
-                        if ($square_cards[$component]['enabled'] && !empty($site_stats['random_link'])):
+                        if (isset($site_stats['random_link']) && !empty($site_stats['random_link'])):
                             $rl = $site_stats['random_link'];
-                            $img = $rl->link_image ?: 
-                                    'https://weavatar.com/avatar/?s=80&d=mm&r=g'; ?>
+                            $img = isset($rl->link_image) && !empty($rl->link_image) ? $rl->link_image : 
+                                   'https://weavatar.com/avatar/?s=80&d=mm&r=g'; ?>
                             <div class="stat-capsule link-capsule">
                             <div class="link-avatar"><img src="<?php echo esc_url($img); ?>" /></div>
                             <div class="link-info">
@@ -392,17 +442,18 @@ if ($show_medal_capsules) {
                         break;
 
                     case 'announcement':
-                        if ($square_cards['announcement']['enabled']): 
-                            // 强制两行显示
+                        // 优化：只有当标签存在时才处理
+                        if (isset($square_cards['announcement']) && isset($square_cards['announcement']['label'])): 
                             $text = $square_cards['announcement']['label'];
+                            // 强制两行显示
                             $lines = preg_split('/<br\s*\/?>|\n/',$text,2);
                             if (count($lines)<2) {
-                            $len = mb_strlen($lines[0]);
-                            $half = ceil($len/2);
-                            $first = mb_substr($lines[0],0,$half);
-                            $second= mb_substr($lines[0],$half);
+                                $len = mb_strlen($lines[0]);
+                                $half = ceil($len/2);
+                                $first = mb_substr($lines[0],0,$half);
+                                $second= mb_substr($lines[0],$half);
                             } else {
-                            list($first,$second) = $lines;
+                                list($first,$second) = $lines;
                             } ?>
                             <div class="stat-capsule announcement-capsule">
                             <i class="<?php echo $square_cards['announcement']['icon']; ?>"></i>
@@ -413,27 +464,35 @@ if ($show_medal_capsules) {
                             </div>
                         <?php endif;
                         break;
-                endswitch; ?>
-            <?php endforeach; ?>
+                endswitch; 
+            endforeach; ?>
         </div>
         <?php endif; ?>
 
         <!-- 展示卡片 -->
         <?php 
         if (!empty($exhibition)) : 
+            // 默认图片路径缓存，避免重复调用iro_opt
+            $default_img_base = null;
+            
             foreach ($exhibition as $item) : 
+                // 跳过非数组项
                 if (!is_array($item)) {
                     continue; 
                 }
                 
-                $link = isset($item['link']) ? $item['link'] : '#';
-                $title = isset($item['title']) ? $item['title'] : '';
-                $description = isset($item['description']) ? $item['description'] : '';
-                $img = isset($item['img']) ? $item['img'] : '';
+                // 使用更高效的数组访问方式
+                $link = $item['link'] ?? '#';
+                $title = $item['title'] ?? '';
+                $description = $item['description'] ?? '';
+                $img = $item['img'] ?? '';
                 
                 // 如果没有图片，使用默认图片
                 if (empty($img)) {
-                    $img = iro_opt('vision_resource_basepath', 'https://s.nmxc.ltd/sakurairo_vision/@3.0/') . 'series/exhibition1.webp';
+                    if ($default_img_base === null) {
+                        $default_img_base = iro_opt('vision_resource_basepath', 'https://s.nmxc.ltd/sakurairo_vision/@3.0/');
+                    }
+                    $img = $default_img_base . 'series/exhibition1.webp';
                 }
                 ?>
                 <div class="bento-item bento-medium">
