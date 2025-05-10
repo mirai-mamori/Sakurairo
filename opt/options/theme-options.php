0aa2f2373924e74f085f71ae04aa0ada8c05ddeb
Tianci Hou <45251727+EnderturtleOrz@users.noreply.github.com>
2025-05-10 21:38:19 +0800

    fix: correct dependency condition for random_graphs_options in theme-options.php
    Updated the dependency condition from 'local' to 'gallery' to ensure the same value. The previous incorrect condition caused the dependency to not trigger as expected.

--- opt/options/theme-options.php
+++ opt/options/theme-options.php
@@ -1652,7 +1652,7 @@ $prefix = 'iro_options';
         'desc' => __('Fill in an URL','sakurairo_csf'),
         'dependency' => array( 
                               array( 'cover_switch', '==', 'true', '', 'true' ),
-                              array( 'random_graphs_options', '!=', 'local', '', 'true' ),
+                              array( 'random_graphs_options', '!=', 'gallery', '', 'true' ),
                         ),
         'default' => 'https://api.fuukei.org/random-img/default/pc.php',
         'sanitize' => false,
@@ -1666,7 +1666,7 @@ $prefix = 'iro_options';
         'dependency' => array( 
                               array( 'random_graphs_mts', '==', 'true' ),
                               array( 'cover_switch', '==', 'true', '', 'true' ),
-                              array( 'random_graphs_options', '!=', 'local', '', 'true' ),
+                              array( 'random_graphs_options', '!=', 'gallery', '', 'true' ),
                         ),
         'desc' => __('Fill in an URL','sakurairo_csf'),
         'default' => 'https://api.fuukei.org/random-img/default/mobile.php',
