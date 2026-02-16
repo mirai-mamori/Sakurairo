f34d82776fd16209e13be2ad588436f693ff5fe2
楠ちゃん <kusu@kusu.moe>
2026-02-16 17:29:51 +0800

    Sync

---
+++ js/3.js
@@ -0,0 +1,2 @@
+(globalThis.webpackChunksakurairo_scripts=globalThis.webpackChunksakurairo_scripts||[]).push([[3],{3(a,r,s){s.r(r),s.d(r,{default:()=>n});var i=s(2740);function n(a){const{label:r,centroid:s}=(0,i.IA)(a,8),n=Math.max(...r);return s[r.findIndex(a=>a==n)]}}}]);
+//# sourceMappingURL=3.js.map
